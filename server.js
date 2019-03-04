1ba0063bd8f10f00f968dd341681a3c80fef6830
Josh Cain <josh@semper.reformanda.io>
2019-03-04 17:12:50 -0600

    remove explicit CORS options, unnecessary

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ const checkJwt = jwt({
 const checkScopes = jwtAuthz([ 'read:messages' ]);
 const checkScopesAdmin = jwtAuthz([ 'write:messages' ]);
 
-app.options('/api/public', cors())
 app.get('/api/public', function(req, res) {
   res.json({ message: "Hello from a public endpoint! You don't need to be authenticated to see this." });
 });
